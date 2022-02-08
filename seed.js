0ccfeda5c07cd4be07a765256e731a9ef2468430
brianna brennan <brianna.brennanj@gmail.com>
2022-02-08 13:25:15 -0800

    Initial data seed

---
+++ seed.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+require('./config/database');
+
+const Category = require('./models/category');
+const Item = require('./models/item');
+
+(async function () {
+	await Category.deleteMany({});
+	const categories = await Category.create([
+		{ name: 'Canvas', sortOrder: 10 },
+		{ name: 'Prints', sortOrder: 20 },
+		{ name: 'Supplies', sortOrder: 30 },
+	]);
+
+	await Item.deleteMany({});
+	const items = await Item.create([
+		{
+			title: 'Sunshine Painting',
+			description: 'Acrylic painting on canvas.',
+			price: 52,
+			category: categories[1],
+		},
+		{
+			title: 'Flower Print',
+			description: '5x10 print on photo paper.',
+			price: 22,
+			category: categories[2],
+		},
+		{
+			title: 'Mermaid Painting',
+			description: '10x14 painting on canvas.',
+			price: 55,
+			category: categories[1],
+		},
+	]);
+
+	console.log(items);
+
+	process.exit();
+})();
